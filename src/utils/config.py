50777236398b0c819c4811fe83770ea25fa847fb
Ringo Hoffmann <rh@digitronic.net>
2018-01-18 09:56:38 +0100

    added config module and config file

---
+++ src/utils/config.py
@@ -0,0 +1,20 @@
+import json
+import os
+from utils.msgs import Msgs
+
+class Config:
+    def __init__(self, path):
+        if not os.path.isfile(path):
+            Msgs.error(
+                "Config file not found!\nPlease Download the file from:\n" +
+                "https://github.com/zekroTJA/serverManager"
+            )
+            exit(1)
+        with open(path, "r") as f:
+            raw = "".join(
+                [l for l in f if not l.replace(" ", "").replace("\t", "").startswith("//")]
+            )
+            self.config = json.loads(raw)
+    
+    def get_config(self):
+        return self.config
